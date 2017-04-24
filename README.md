fd9e48f047226f5682809985d50e932b49e9dae1
sergei <kho.sergey@gmail.com>
2017-04-23 23:22:54 -0400

    created deep copy function, and now successfully looping over all characters. need to restore elements to the array once it loops

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 beale cipher encryption and decryption algorithms
 
 ## In Progress:
-* currently working on changing to mono alphabetic state to a polyalphabetical one which is the essence of the beal cipher.
+* currently working on changing the mono alphabetic encryption to a polyalphabetical one which is the essence of the beal cipher.
--- encryption.js
+++ encryption.js
@@ -11,6 +11,17 @@ if(process.argv.length < 3){
 var textForSource = process.argv[2];
 var textToEncrypt = process.argv[3];
 
+/*function to make a deep copy of an array*/
+function createCopy(arrToCopy, newArray){
+	var newArray = [];
+	for(var i = 0; i < arrToCopy.length; i++){
+		newArray.push(arrToCopy[i]);
+		if(i == arrToCopy.length-1){
+			return newArray;
+		}
+	}
+}
+
 function encrypt(plainText,sourceText){
 
 	//****Reading plain text
@@ -18,7 +29,7 @@ function encrypt(plainText,sourceText){
 
 	//**** Split the words by whitespace and throw it in array****//
 	var sourceWords = source.split(" ");
-	var sourceFinal = []
+	var sourceFinal = [];
 	for(var i = 0; i <sourceWords.length; i++){
 	  var sourceLetter = sourceWords[i].split('')[0];
 	  sourceFinal.push(sourceLetter);
@@ -37,24 +48,34 @@ function encrypt(plainText,sourceText){
 	* return index + ","
 	***/
 
-
-	sourceText = sourceFinal;
-	plainText = plainFinal;
+	//make a copy of the source book text so we wont polute it.
+	var sourceText = createCopy(sourceFinal, sourceText);
+	
+	//var sourceText = sourceFinal;
+	var plainText = plainFinal;
+	
+	var usedArr = [];
 	var encArr = [];
 	for(var i = 0; i < plainText.length; i++){
 	  var plainLetter = plainText[i];
 	  for(var j = 0; j < sourceText.length; j++){
 	    var sourceLetter = sourceText[j];
 	    if(sourceLetter === plainLetter){
-	      var tempLetter = j;
-	      encArr.push(tempLetter);
+	      encArr.push(j);
+	      delete sourceText[j];
 	      break;
-	    }
-	   
-	  }
+	    }else if( j === sourceText.length-1){ 
+				for(var k = 0; k < sourceFinal.length; k++){
+					if(sourceFinal[k] === plainLetter){
+						encArr.push(k);
+						break;
+					}
+		   	}
+	 	 }
+		}
 	}
 	return encArr.toString();
+
 }
 
 console.log(encrypt(textToEncrypt,textForSource));
-
