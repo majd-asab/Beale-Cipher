2ca4482e116f51005549da805bd74216d5927ac2
Majd <kho.sergey@gmail.com>
2017-04-27 20:16:25 -0400

    missing: replacing letters that  the source book
    it replaces with the whole plain-text and not individual letter

--- encryption.js
+++ encryption.js
@@ -1,8 +1,8 @@
-//****modules
+//modules
 var fs = require("fs");
 
 
-//****Checking for CLA
+//Checking for command line arguments
 if(process.argv.length < 3){
   console.log("Usage: node " + __filename + " SourceText + PlainText");
   process.exit(-1);
@@ -11,7 +11,7 @@ if(process.argv.length < 3){
 var textForSource = process.argv[2];
 var textToEncrypt = process.argv[3];
 
-/*function to make a deep copy of an array*/
+//function to make a deep copy of an array
 function createCopy(arrToCopy, newArray){
 	var newArray = [];
 	for(var i = 0; i < arrToCopy.length; i++){
@@ -24,10 +24,10 @@ function createCopy(arrToCopy, newArray){
 
 function encrypt(plainText,sourceText){
 
-	//****Reading plain text
+	//Reading the source book (letters used to encrypt with) from buffer into utf8
 	var source = fs.readFileSync(textForSource,'UTF8');
 
-	//**** Split the words by whitespace and throw it in array****//
+	// Splitting the words by whitespace and throwing them into an array
 	var sourceWords = source.split(" ");
 	var sourceFinal = [];
 	for(var i = 0; i <sourceWords.length; i++){
@@ -36,7 +36,7 @@ function encrypt(plainText,sourceText){
 	}
 	 //console.log(sourceFinal);
 
-	//**** Reading the text to encrypt different
+	// Reading the text to encrypt different
 	var plainText = fs.readFileSync(textToEncrypt, 'UTF8');
 	var plainletter = plainText.replace(/[^a-zA-Z0-9]/g,'');
 	var plainFinal = plainletter.split("");
